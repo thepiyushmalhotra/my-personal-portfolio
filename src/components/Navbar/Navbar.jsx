924180ffa4db3f1428d0615143e299d5e5a155dc
Piyush Malhotra <piyush@soulber.co>
2022-02-25 21:50:19 -0500

    setup starter files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/container/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './About.scss';
+
+const About = () => {
+  return (
+    <div>About</div>
+  );
+};
+
+export default About;
