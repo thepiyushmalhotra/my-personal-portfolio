031da25a518c711a06022593b08379d911031edf
Piyush Malhotra <piyush@soulber.co>
2022-03-07 15:33:10 -0500

    add wrappers

---
+++ src/wrapper/AppWrap.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavigationDots, SocialMedia } from "../components";
+
+const AppWrap = (Component, idName, classNames) =>
+  function HOC() {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+        <div className="app__wrapper app__flex">
+          <Component />
+
+          <div className="copyright">
+            <p className="p-text">Piyush Malhotra</p>
+            <p className="p-text">Full Stack Developer</p>
+          </div>
+        </div>
+        <NavigationDots active={idName} />
+      </div>
+    );
+  };
+
+export default AppWrap;
